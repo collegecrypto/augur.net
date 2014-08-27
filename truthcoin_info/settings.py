689611617666d41ed54f444ed46a73d4e475a6a3
Scott Leonard <scott@mixwe.com>
2014-08-26 20:19:19 -0700

    - setup production static file serving

--- truthcoin_info/settings.py
+++ truthcoin_info/settings.py
@@ -5,6 +5,8 @@ import os
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
@@ -63,7 +65,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'truthcoin_info', 'staticfiles')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
--- truthcoin_info/urls.py
+++ truthcoin_info/urls.py
@@ -1,5 +1,12 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView, RedirectView
+from django.conf import settings
 
+class TextPlainView(TemplateView):
+  def render_to_response(self, context, **kwargs):
+    return super(TextPlainView, self).render_to_response(context, content_type='text/plain', **kwargs)
+
+    
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
@@ -15,4 +22,9 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+
+    # django static
+  	url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT, 'show_indexes': True}),
+  	url(r'^robots\.txt$', TextPlainView.as_view(template_name='robots.txt')),
+  	url(r'^favicon\.ico$', RedirectView.as_view(url='%simages/favicon.png' % settings.STATIC_URL)),
 )
