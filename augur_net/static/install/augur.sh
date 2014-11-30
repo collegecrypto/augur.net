240e536d19b9f230ed495b88524ca2884660f955
Jack Peterson <jack@tinybike.net>
2014-11-29 19:20:57 -0800

    removed sudo from osx install

--- augur_net/static/install/augur.sh
+++ augur_net/static/install/augur.sh
@@ -4,6 +4,6 @@ killall Python
 rm -rf augur
 git clone https://github.com/AugurProject/augur.git
 cd augur
-sudo pip install -r requirements.txt
+pip install -r requirements.txt
 echo "Augur started, go to http://127.0.0.1:9000/ in your web browser"
 ./augur_ctl start
